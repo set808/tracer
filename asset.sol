c6896a52a896cddec4713dde103bbf57e8135d9c
gjdame <gregoryjdame@gmail.com>
2018-04-11 15:33:48 -0700

    asset file allows to set & get asset information

---
+++ asset.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.2;
+
+contract guns {
+    
+    struct asset {
+        uint serial;
+        bytes32 uuid;
+        string owner;
+        string manufact;
+        bool initialized;
+    }
+    
+    mapping (address => asset) assets;
+    address[] public assetAccts;
+    
+    function setAsset(address _address, uint _serial, string _manufact, string _owner, bytes32 _uuid, bool _initialized) public {
+        var asset = assets[_address];
+        
+        asset.serial = _serial;
+        asset.uuid = _uuid;
+        asset.owner = _owner;
+        asset.manufact = _manufact;
+        asset.initialized = _initialized;
+        
+        assetAccts.push(_address) -1;
+    }
+    
+    function getAssets() view public returns(address[]) {
+        return assetAccts;
+    }
+    
+    function getAsset(address _address) view public returns(uint, string, string, bytes32, bool) {
+        return (assets[_address].serial, assets[_address].manufact, assets[_address].owner, assets[_address].uuid, assets[_address].initialized);
+    }
+    
+}
