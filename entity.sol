f8f47ed0376936514011d9802c766f42c2bb4c13
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-16 04:21:46 +0000

    Rework entity.sol

--- entity.sol
+++ entity.sol
@@ -1,91 +1,45 @@
 pragma solidity ^0.4.2;
 
+pragma solidity ^0.4.2;
+
 contract Owner
 {
-    struct owner
+    bytes16 public name;
+    uint public age;
+    address public own_add;
+    
+    function Owner(bytes16 _name, uint _age) public
     {
-        string name;
-        uint age;
-        
-    }
-    
-    mapping (address => owner) owners;
-    address[] public ownerAccts;
-    
-    function setOwner(string _name, uint _age) public
-    {
-        owner storage this_owner = owners[this];
-        this_owner.name = _name;
-        this_owner.age = _age;
-        
-        ownerAccts.push(this) + 1;
-    }
-    
-    function getOwners() view public returns(address[]) {
-        return ownerAccts;
-    }
-    
-    function getOwner(address _address) view public returns(string, uint) {
-        return (owners[_address].name, owners[_address].age);
+        name = _name;
+        age = _age;
+        own_add = this;
     }
 }
 
 contract Dealer
 {
-    struct dealer
+    bytes16 public name;
+    bool public ffl;
+    address public deal_add;
+    
+    function Dealer(bytes16 _name, bool _ffl) public
     {
-        string name;
-        bool ffl;
-        
-    }
-    
-    mapping (address => dealer) dealers;
-    address[] public dealerAccts;
-    
-    function setDealer(string _name, bool _ffl) public
-    {
-        dealer storage this_dealer = dealers[this];
-        this_dealer.name = _name;
-        this_dealer.ffl = _ffl;
-        
-        dealerAccts.push(this) + 1;
-    }
-    
-    function getDealers() view public returns(address[]) {
-        return dealerAccts;
-    }
-    
-    function getDealer(address _address) view public returns(string, uint) {
-        return (dealers[_address].name, dealers[_address].ffl);
+        name = _name;
+        ffl = _ffl;
+        deal_add = this;
     }
 }
 
 contract Manufacturer
 {
-    struct manufacturer
+    bytes16 name;
+    bool ffl;
+    address deal_add;
+    
+    function Manufacturer(bytes16 _name, bool _ffl) public
     {
-        string name;
-        bool ffl;
-        
-    }
-    
-    mapping (address => manufacturer) manufacturers;
-    address[] public manufacturerAccts;
-    
-    function setManufacturer(string _name, bool _ffl) public
-    {
-        manufacturer storage this_manufacturer = manufacturers[this];
-        this_manufacturer.name = _name;
-        this_manufacturer.ffl = _ffl;
-        
-        manufacturerAccts.push(this) + 1;
-    }
-    
-    function getManufacturers() view public returns(address[]) {
-        return manufacturerAccts;
-    }
-    
-    function getManufacturer(address _address) view public returns(string, uint) {
-        return (manufacturers[_address].name, manufacturers[_address].age);
+        name = _name;
+        ffl = _ffl;
+        deal_add = this;
     }
 }
