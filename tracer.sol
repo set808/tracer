ae0e9b2d28e067c9db3286cbd65aec8d9a3ae5a5
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-16 04:23:33 +0000

    Update tracer.sol

--- tracer.sol
+++ tracer.sol
@@ -1,30 +1,73 @@
 pragma solidity ^0.4.2;
 
-function createAsset(string name, string description, string uuid, string manufacturer) {
- 
-    if(assetStore[uuid].initialized) {
-        RejectCreate(msg.sender, uuid, "Asset with this UUID already exists.");
-        return;
-      }
- 
-      assetStore[uuid] = Asset(name, description, true, manufact);
-      walletStore[msg.sender][uuid] = true;
-      AssetCreate(msg.sender, uuid, manufact);
-}
+pragma solidity ^0.4.2;
+import "./entity.sol";
+import "./asset.sol";
 
-function transferAsset(address to, string uuid) {
- 
-    if(!assetStore[uuid].initialized) {
-        RejectTransfer(msg.sender, to, uuid, "No asset with this UUID exists");
-        return;
+contract Tracer
+{
+        address[] ownerAccts;
+        address[] dealerAccts;
+        address[] manufacturerAccts;
+    
+    mapping (address => Owner) owners;
+    mapping (address => Dealer) dealers;
+    mapping (address => Manufacturer) manufacturers;
+    mapping (bytes32 => guns) assetStore;
+    mapping (address => mapping(bytes32 => bool)) public entityStore;
+    
+    function createOwner(bytes16 _name, uint _age) public
+    {
+        Owner new_owner = new Owner(_name, _age);
+        owners[new_owner] = new_owner;
+        ownerAccts.push(new_owner) - 1;
     }
- 
-    if(!walletStore[msg.sender][uuid]) {
-        RejectTransfer(msg.sender, to, uuid, "Sender does not own this asset.");
-        return;
+    function createDealer(bytes16 _name, bool _ffl) public
+    {
+        Dealer new_dealer = new Dealer(_name, _ffl);
+        dealers[new_dealer] = new_dealer;
+        dealerAccts.push(new_dealer) - 1;
     }
- 
-    walletStore[msg.sender][uuid] = false;
-    walletStore[to][uuid] = true;
-    AssetTransfer(msg.sender, to, uuid);
+    function createManufacturer(bytes16 _name, bool _ffl) public
+    {
+        Manufacturer new_man = new Manufacturer(_name, _ffl);
+        manufacturers[new_man] = new_man;
+        manufacturerAccts.push(new_man) - 1;
+    }
+
+    function createAsset(bytes16 _gun_type, bytes16 _manufacturer, bool _initialized) public
+    {
+        guns new_asset =  new guns();
+        new_asset.setAsset(new_asset, _gun_type, _manufacturer, _initialized);
+        bytes32 uuid = new_asset.getUuid(new_asset);
+        assetStore[uuid] = new_asset;
+        entityStore[msg.sender][uuid] = true;
+    }
+    
+    function getOwners() constant public returns (address[])
+    {
+        return ownerAccts;
+    }
+    function getDealers() constant public returns (address[])
+    {
+        return dealerAccts;
+    }
+    function getManufacturers() constant public returns (address[])
+    {
+        return manufacturerAccts;
+    }
+    
+    function getOwner(address ins) constant public returns (bytes16, uint)
+    {
+        return (owners[ins].name(), owners[ins].age());
+    }
+    function isOwnerOf(address owner, bytes32 uuid) constant public returns (bool)
+    {
+        if (entityStore[owner][uuid])
+        {
+            return true;
+        }
+        
+        return false;
+    }
 }
